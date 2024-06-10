0b71923b1df51efceb2c94c6001085169e6fd781
Jairo <ti43903@uvp.edu.mx>
2024-06-10 17:23:28 -0600

    Terminado

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import plugin from "tailwindcss";
+
+export default defineConfig ({
+  plugin: [react()],
+  base:'https://aestractic.github.io/app-heaven/'
+})
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
